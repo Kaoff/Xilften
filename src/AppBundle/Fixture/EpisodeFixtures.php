0454dd54506840bfda81a626841f7a589b46a711
Bastien Weber <bastien@wetshark.fr>
2018-02-26 11:21:55 +0100

    MovieFixtures + EpisodeFixtures fix

--- src/AppBundle/Fixture/EpisodeFixtures.php
+++ src/AppBundle/Fixture/EpisodeFixtures.php
@@ -51,7 +51,7 @@ class EpisodeFixtures extends Fixture implements DependentFixtureInterface
                 for ($k = 0; $k < 12; ++$k)
                 {
                     $season = $this->getReference('tvshow-'.$i.'season-'.$j);
-                    $this->episodeManager->createEpisode($season, $episodeNames[random_int(0, 1000)],'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+                    $this->episodeManager->createEpisode($season, $episodeNames[random_int(0, 999)],'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
                 }
             }
         }
