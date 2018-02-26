2bee9de25b67cbb4adad9acad0d5ff99a59806fb
morganecautto <morganecautto@gmail.com>
2018-02-26 17:15:10 +0100

    creation des entities

--- src/AppBundle/Entity/Movie.php
+++ src/AppBundle/Entity/Movie.php
@@ -23,6 +23,23 @@ class Movie
     private $upvotes;
 
     /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Actor")
+     */
+    private $actors;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Category")
+     */
+    private $categories;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Director")
+     */
+    private $directors;
+
+//    ********* GET/SET *********
+
+    /**
      * Set upvotes
      *
      * @param integer $upvotes
@@ -42,5 +59,116 @@ class Movie
     public function getUpvotes()
     {
         return $this->upvotes;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->actors = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->categories = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->directors = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add actor
+     *
+     * @param \AppBundle\Entity\Actor $actor
+     *
+     * @return Movie
+     */
+    public function addActor(\AppBundle\Entity\Actor $actor)
+    {
+        $this->actors[] = $actor;
+
+        return $this;
+    }
+
+    /**
+     * Remove actor
+     *
+     * @param \AppBundle\Entity\Actor $actor
+     */
+    public function removeActor(\AppBundle\Entity\Actor $actor)
+    {
+        $this->actors->removeElement($actor);
+    }
+
+    /**
+     * Get actors
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getActors()
+    {
+        return $this->actors;
+    }
+
+    /**
+     * Add category
+     *
+     * @param \AppBundle\Entity\Category $category
+     *
+     * @return Movie
+     */
+    public function addCategory(\AppBundle\Entity\Category $category)
+    {
+        $this->categories[] = $category;
+
+        return $this;
+    }
+
+    /**
+     * Remove category
+     *
+     * @param \AppBundle\Entity\Category $category
+     */
+    public function removeCategory(\AppBundle\Entity\Category $category)
+    {
+        $this->categories->removeElement($category);
+    }
+
+    /**
+     * Get categories
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+
+    /**
+     * Add director
+     *
+     * @param \AppBundle\Entity\Director $director
+     *
+     * @return Movie
+     */
+    public function addDirector(\AppBundle\Entity\Director $director)
+    {
+        $this->directors[] = $director;
+
+        return $this;
+    }
+
+    /**
+     * Remove director
+     *
+     * @param \AppBundle\Entity\Director $director
+     */
+    public function removeDirector(\AppBundle\Entity\Director $director)
+    {
+        $this->directors->removeElement($director);
+    }
+
+    /**
+     * Get directors
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getDirectors()
+    {
+        return $this->directors;
     }
 }
--- src/AppBundle/Entity/TVShow.php
+++ src/AppBundle/Entity/TVShow.php
@@ -50,6 +50,22 @@ class TVShow
      */
     private $upvotes;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Actor")
+     */
+    private $actors;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Category")
+     */
+    private $categories;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Director")
+     */
+    private $directors;
+
+
 //    ********* GET/SET *********
 
     /**
@@ -172,5 +188,107 @@ class TVShow
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Add actor
+     *
+     * @param \AppBundle\Entity\Actor $actor
+     *
+     * @return TVShow
+     */
+    public function addActor(\AppBundle\Entity\Actor $actor)
+    {
+        $this->actors[] = $actor;
+
+        return $this;
+    }
+
+    /**
+     * Remove actor
+     *
+     * @param \AppBundle\Entity\Actor $actor
+     */
+    public function removeActor(\AppBundle\Entity\Actor $actor)
+    {
+        $this->actors->removeElement($actor);
+    }
+
+    /**
+     * Get actors
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getActors()
+    {
+        return $this->actors;
+    }
+
+    /**
+     * Add category
+     *
+     * @param \AppBundle\Entity\Category $category
+     *
+     * @return TVShow
+     */
+    public function addCategory(\AppBundle\Entity\Category $category)
+    {
+        $this->categories[] = $category;
+
+        return $this;
+    }
+
+    /**
+     * Remove category
+     *
+     * @param \AppBundle\Entity\Category $category
+     */
+    public function removeCategory(\AppBundle\Entity\Category $category)
+    {
+        $this->categories->removeElement($category);
+    }
+
+    /**
+     * Get categories
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+
+    /**
+     * Add director
+     *
+     * @param \AppBundle\Entity\Director $director
+     *
+     * @return TVShow
+     */
+    public function addDirector(\AppBundle\Entity\Director $director)
+    {
+        $this->directors[] = $director;
+
+        return $this;
+    }
+
+    /**
+     * Remove director
+     *
+     * @param \AppBundle\Entity\Director $director
+     */
+    public function removeDirector(\AppBundle\Entity\Director $director)
+    {
+        $this->directors->removeElement($director);
+    }
+
+    /**
+     * Get directors
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getDirectors()
+    {
+        return $this->directors;
     }
 }
