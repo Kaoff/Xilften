c98d3aeb6ab0bb1d0919c7ead8bd012a1848785a
morganecautto <morganecautto@gmail.com>
2018-02-27 10:15:20 +0100

    logout

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -29,4 +29,10 @@ class SecurityController extends Controller
             'error' => $error
         ]);
     }
+    /**
+     *
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {/*Nothing to do here*/}
 }
